9c33ce2a815423f4a12a20e57990bc9157b1a73f
madhugarugula <40380394+madhugarugula@users.noreply.github.com>
2018-07-11 23:13:28 +0530

    Add files via upload

---
+++ SpringBootApplicationdemo/src/main/java/com/example/SpringBootApplicationdemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationdemoApplication.class, args);
+	}
+}
